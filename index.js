a5c5010a21724cc5d082e8075f4e1d54390affd9
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-04-22 22:43:02 -0300

    ajust to use cpf list

--- index.js
+++ index.js
@@ -3,22 +3,23 @@
 
 const puppeteer = require("puppeteer");
 let cpfConsultado = "986.108.960-80"; // cpf gerado na api 4devs
+let cpfs = ["986.108.960-80", "626.974.070-30"]; // cpf gerado na api 4devs
 
-const consultaCpf = async cpf => {
-  const browser = await puppeteer.launch({
-    headless: false
-  });
+const consultaCpf = async cpfs => {
+  for (let i = 0; i < cpfs.length; i++) {
+    const browser = await puppeteer.launch({
+      headless: true
+    });
+    const page = await browser.newPage();
+    await page.goto("https://www.situacao-cadastral.com/");
 
-  const page = await browser.newPage();
-  await page.goto("https://www.situacao-cadastral.com/");
-
-  await page.waitFor('input[name="doc"]');
-  await page.type('input[name="doc"]', cpf, { delay: 180 });
-  await page.keyboard.press("Enter");
-  await page.waitFor("#corpo > tbody > tr:nth-child(2) > td > span > a");
-  await page.screenshot({ path: `images/consultaCPF-${cpf}.png` });
-
-  await browser.close();
+    await page.waitFor('input[name="doc"]');
+    await page.type('input[name="doc"]', cpfs[i], { delay: 180 });
+    await page.keyboard.press("Enter");
+    await page.waitFor("#corpo > tbody > tr:nth-child(2) > td > span > a");
+    await page.screenshot({ path: `images/consultaCPF-${cpfs[i]}.png` });
+    await browser.close();
+  }
 };
 
-consultaCpf(cpfConsultado);
+consultaCpf(cpfs);
